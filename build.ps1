43e115e588b7355a7448ed4975354c742abf9976
Stefan Scherer <stefan.scherer@docker.com>
2021-06-09 09:42:12 +0200

    Create image for specific arch

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ Write-Host Starting build
 if ($isWindows) {
   docker build --pull -t whoami -f Dockerfile.windows .
 } else {
-  docker build -t whoami --build-arg "arch=$env:ARCH" .
+  docker build --platform "linux/$env:ARCH" -t whoami --build-arg "arch=$env:ARCH" .
 }
 
 docker images
