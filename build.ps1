ddade5c0db6f146dbe6c4ecce7579de28777d880
Stefan Scherer <stefan.scherer@docker.com>
2021-06-09 10:13:35 +0200

    Pull amd64 golang image

--- build.ps1
+++ build.ps1
@@ -5,6 +5,7 @@ Write-Host Starting build
 if ($isWindows) {
   docker build --pull -t whoami -f Dockerfile.windows .
 } else {
+  docker pull golang
   docker build --platform "linux/$env:ARCH" -t whoami --build-arg "arch=$env:ARCH" .
 }
 
